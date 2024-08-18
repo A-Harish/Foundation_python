2d41f467ecdbfcc9ff90d28cf13d219c730415ad
A-Harish <162663431+A-Harish@users.noreply.github.com>
2024-08-18 12:35:19 +0530

    if else ... elif .... switch

---
+++ if_elif_elseif_switch.py
@@ -0,0 +1,91 @@
+# # If stmts
+# # weather = input('whats the weather today : ')
+
+# # if weather == "sunny":
+# #     print('you can go and play your faviorite game...')
+
+
+
+
+
+
+# # # If else stmts
+# # weather = input('whats the weather today : ')
+
+# # if weather == "sunny":
+# #     print('you can go and play your faviorite game...')
+
+# # else:
+# #     print('keep quite...')
+
+
+
+
+
+# # elif  stmts
+# # weather = input('whats the weather today : ')
+
+# # if weather == "sunny":
+# #     print('you can go and play your faviorite game...')
+
+# # elif weather == 'rainy':
+# #     print('its raining there...')
+
+# # else:
+#     # print('seems unconditional go and sleep...')
+
+
+
+# # nested stmts
+# # weather = input('whats the weather today : ')
+# # day_of_time = input('confirm me day or night : ')
+# # if weather == "sunny":
+# #     print('you can go and play your faviorite game...')
+# #     if day_of_time == 'day':
+# #         print('yeah you can go now and play')
+
+# # elif weather == 'rainy':
+# #     print('its raining there...')
+
+# # else:
+# #     print('seems unconditional go and sleep...')
+
+
+# # Q ---- Simple calculator program 
+
+# a = int(input('enter values of a : '))
+# b = int(input('enter values of b : '))
+
+# dission = input('which operation you want to do ? ')
+# if dission == 'add':
+#     res = a+b
+# elif dission == 'mul':
+#     res = a*b
+
+# elif dission == 'div':
+#     res = a/b
+# elif dission == 'sub':
+#     res = a-b
+# else:
+#     res = 'invalid input ::'
+# print(f'{dission} values of {a} and {b} are {res}')
+
+
+
+
+# if __name__ == '__main__':
+#     fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+#     ar_count = int(input().strip())
+
+#     ar = list(map(int, input().rstrip().split()))
+
+#     # result = simpleArraySum(ar)
+
+#     # fptr.write(str(result) + '\n')
+
+#     # fptr.close()
+
+
+
+
